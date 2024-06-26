3d92426f6716cd4194c3837896ba33bfa173a5bd
Ram Yewale <ramn.yewale@gmail.com>
2024-06-26 23:56:24 +0530

    chore: Initial commit

---
+++ src/test/java/com/java/tdd/TestDrivenDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
