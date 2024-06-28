b14bcea5a8e30ff60d5a5572578215c4eedc2339
Ram Yewale <ramn.yewale@gmail.com>
2024-06-29 00:44:42 +0530

    chore: Initial commit

--- src/main/java/com/java/tdd/controller/PhotoController.java
+++ src/main/java/com/java/tdd/controller/PhotoController.java
@@ -1,8 +1,10 @@
 package com.java.tdd.controller;
 
 import com.java.tdd.model.Photo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +17,34 @@ public class PhotoController {
     public List<Photo> getAllPhotos() {
         return List.of(new Photo(1, "Test Album", 11, "https://testthumbnail.png"),
                 new Photo(2, "Test Album", 12, "https://testthumbnail.jpg"));
+    }
+
+    @GetMapping("/population")
+    public ResponseEntity<?> getPopulationCountByCityName(@RequestParam(required = false) String cityName) {
+        if (cityName.equalsIgnoreCase("invalidCityName")) {
+            return ResponseEntity.badRequest().body("Request failed due to invalid city name provided");
+        }
+
+        Long population = getpopulation(cityName);
+
+        if (cityName.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(population);
+    }
+
+    private Long getpopulation(String cityName) {
+        Long population;
+        if (cityName == null || cityName.isEmpty()) { // Use isEmpty() to check for empty string
+            return null;
+        } else {
+            if (cityName.equalsIgnoreCase("Pune"))
+                population = 123456789L;
+            else if (cityName.equalsIgnoreCase("Delhi"))
+                population = 356982456L;
+            else
+                population = null;
+        }
+        return population;
     }
 }
--- src/test/java/com/java/tdd/PhotoControllerIT.java
+++ src/test/java/com/java/tdd/PhotoControllerIT.java
@@ -1,17 +1,19 @@
 package com.java.tdd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.java.tdd.controller.PhotoController;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.client.RequestMatcher;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.regex.Matcher;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PhotoController.class)
 @AutoConfigureMockMvc
@@ -22,8 +24,42 @@ public class PhotoControllerIT {
 
     @Test
     void shouldFetchAllPhotos() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/v1/photos"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+        mockMvc.perform(get("/v1/photos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(2)));
+    }
+
+    @Test
+    void shouldReturnPopulationByCityName() throws Exception {
+        Long expected = 123456789L;
+        String response = mockMvc.perform(get("/v1/population")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .param("cityName", "Pune"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Long population = objectMapper.readValue(response, Long.class);
+        assertEquals(expected, population);
+    }
+
+    @Test
+    void shouldReturnPopulationByCityName_ThrowsException_IfInvalidCityNameProvided() throws Exception {
+        String cityName = "";
+        this.mockMvc.perform(get("/v1/population")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .param("cityName", cityName))
+                .andExpect(status().isNoContent());
+
+        String response = this.mockMvc.perform(get("/v1/population")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .param("cityName", "invalidCityName"))
+                .andExpect(status().isBadRequest())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        assertEquals(response, "Request failed due to invalid city name provided");
     }
 }
